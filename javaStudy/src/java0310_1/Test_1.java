764033c51d449b98e0596413dc40643411afe84e
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-10 18:46:04 +0900

    별 테스트

---
+++ javaStudy/src/java0310_1/Test_1.java
@@ -0,0 +1,15 @@
+package java0310_1;
+import java.util.Scanner;
+public class Test_1 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<11; i++) {
+			for(int g=1; g<=i; g++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
---
+++ javaStudy/src/java0310_1/Test_2.java
@@ -0,0 +1,15 @@
+package java0310_1;
+
+public class Test_2 {
+
+	public static void main(String[] args) {
+		for(int i = 10; i>0; i--) {
+		    for(int g = i; g>1; g--) {
+		    	System.out.print("*");
+		    }
+		    System.out.println();
+		}
+
+	}
+
+}
