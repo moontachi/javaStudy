542c8c4ba4479c99c50fbe212c626828557eb2ff
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-14 18:12:11 +0900

    up&down game

---
+++ javaStudy/src/java0313_1/Exam_7.java
@@ -0,0 +1,29 @@
+package java0313_1;
+import java.util.Scanner;
+
+public class Exam_7 {
+     public static void main(String[] args) {
+     int answer = (int)(Math.random()*100);
+     System.out.println("1~100 사이의 숫자를 골랐으니 숫자를 맞춰 보시오.");
+     Scanner scanner = new Scanner(System.in);
+          
+          int i=0;  
+          while(true){
+               System.out.print("숫자 입력 >");
+               int sn = scanner.nextInt();
+                 
+               if(answer != sn && answer > sn){
+                    System.out.println("응 아니야~~ 더커~~");
+                    i++;
+                  
+               }else if(answer != sn && answer < sn){
+                    System.out.println("응 아니야~~ 더작아~~");
+                    i++;
+               }else if(answer == sn){
+                    System.out.println("ㅅㄱ여 밥먹으로감"+i+"손안아프냐?");
+                    System.exit(0); 
+               }
+          }
+          
+     }
+}
