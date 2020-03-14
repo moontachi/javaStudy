be4e6b1e02ecb149b48877de5364656067ce3100
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-14 18:11:52 +0900

    이중for문+if

---
+++ javaStudy/src/java0313_1/Exam_6.java
@@ -0,0 +1,19 @@
+package java0313_1;
+
+
+public class Exam_6 {
+
+	public static void main(String[] args) {
+		for(int x=1; x<=10; x++) {
+			for(int y=1; y<=10; y++) {
+				if(x*2+y*5==30) {
+					System.out.println(x+" "+y);
+				}
+				
+			}
+		}
+
+	} 
+
+	
+}
