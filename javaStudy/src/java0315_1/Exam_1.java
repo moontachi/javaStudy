7403c63ef7d8157a3909c2cfb7cc52bf116a2b96
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-15 23:58:21 +0900

    1차원 배열

---
+++ javaStudy/src/java0315_1/Exam_1.java
@@ -0,0 +1,24 @@
+package java0315_1;
+
+public class Exam_1 {
+
+	public static void main(String[] args) {
+			//1차원 배열
+			int[] array1 = new int[100];
+			
+			array1[0] = 50;
+			array1[10] = 100;
+			
+			int[] array2 = new int[] {1,2,3,4};
+			
+			int[] array3 = {1,2,3,4};
+			
+			System.out.println(array3[3]);
+			int value = array3[0];
+			System.out.println(value);
+			
+			System.out.println(array2[2]);
+			
+	}
+
+}
