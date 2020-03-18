0812557037e28b0ae77522764a1b22979b995eeb
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-18 16:02:39 +0900

    기본/참조 타입

---
+++ javaStudy/src/java0318_1/Exam_2.java
@@ -0,0 +1,13 @@
+package java0318_1;
+
+public class Exam_2 {
+
+	public static void main(String[] args) {
+		int i = 4; //기본타입
+		
+		String str = new String("hello");
+		//참조형 타입  str -> hello 라는 String 객체
+
+	}
+
+}
