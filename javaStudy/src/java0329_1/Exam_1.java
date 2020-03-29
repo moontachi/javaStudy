78b1c6064679537d66e87fd2e2cf2c8e9e4e5c9f
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-29 10:26:46 +0900

    생성자/this/생성자 오버로딩

--- javaStudy/src/java0329_1/Exam_1.java
+++ javaStudy/src/java0329_1/Exam_1.java
@@ -1,5 +1,5 @@
 package java0329_1;
-//생성자 , this
+//생성자 , this, 생성자 오버로딩
 public class Exam_1 {
 	//타입 필드명
 	String name;
@@ -10,6 +10,17 @@ public class Exam_1 {
 		//this <- 나의 구성 요소. 내필드 name 에다가 지금들어온 name을 넣어주세요.
 		//객체 자신을 참조하는 키워드.
 		//변수명은 직관적으로 설정.
+	}
+	
+	public Exam_1() {
+//		this.name = "이름없음";
+//		this.number = 0;
+		this("이름없음",0); // 자신의 생성자를 호출 public Exam_1(String name, int number)
+	}
+	
+	public Exam_1(String name, int number) {
+		this.name = name;
+		this.number = number;
 	}
 
 }
--- javaStudy/src/java0329_1/Exam_2.java
+++ javaStudy/src/java0329_1/Exam_2.java
@@ -1,13 +1,18 @@
 package java0329_1;
 
 public class Exam_2 {
-//생성자
+//생성자, 생성자 오버로딩
 	public static void main(String[] args) {
 		Exam_1 c1 = new Exam_1("람보르기니");
 		Exam_1 c2 = new Exam_1("포르쉐");
+		Exam_1 c3 = new Exam_1("bmw", 1234);
+		Exam_1 c4 = new Exam_1();
 		
 		System.out.println(c1.name);
 		System.out.println(c2.name);
+		System.out.println(c3.name + c3.number);
+		System.out.println(c4.name + c4.number);
+		
 
 	}
 
