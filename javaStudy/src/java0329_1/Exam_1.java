71b47c1dfb0c76f633265835d5c13a9467405494
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-29 09:40:25 +0900

    생성자

---
+++ javaStudy/src/java0329_1/Exam_1.java
@@ -0,0 +1,12 @@
+package java0329_1;
+//생성자
+public class Exam_1 {
+	//타입 필드명
+	String name;
+	int number;
+	
+	public Exam_1(String n) {
+		name = n;
+	}
+
+}
---
+++ javaStudy/src/java0329_1/Exam_2.java
@@ -0,0 +1,14 @@
+package java0329_1;
+
+public class Exam_2 {
+//생성자
+	public static void main(String[] args) {
+		Exam_1 c1 = new Exam_1("람보르기니");
+		Exam_1 c2 = new Exam_1("포르쉐");
+		
+		System.out.println(c1.name);
+		System.out.println(c2.name);
+
+	}
+
+}
