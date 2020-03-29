92c78fc9c8c5af3165a83ae784098ccd2d285e30
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-29 09:46:44 +0900

    생성자 / this

--- javaStudy/src/java0329_1/Exam_1.java
+++ javaStudy/src/java0329_1/Exam_1.java
@@ -1,12 +1,14 @@
 package java0329_1;
-//생성자
+//생성자 , this
 public class Exam_1 {
 	//타입 필드명
 	String name;
 	int number;
 	
-	public Exam_1(String n) {
-		name = n;
+	public Exam_1(String name) {
+		this.name = name;
+		//this <- 나의 구성 요소. 내필드 name 에다가 지금들어온 name을 넣어주세요.
+		//객체 자신을 참조하는 키워드.
 	}
 
 }
