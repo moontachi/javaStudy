c511ab0a6e4b9ce58c195f2d8ba9f556380d3d3a
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-29 11:19:49 +0900

    접근제한자

---
+++ javaStudy/src/java0329_3/AccessObjExam.java
@@ -0,0 +1,14 @@
+package java0329_3;
+
+public class AccessObjExam {
+
+	public static void main(String[] args) {
+		AccessObj obj = new AccessObj();
+		System.out.println(obj.p);
+		System.out.println(obj.p2);
+		System.out.println(obj.p3);
+//		System.out.println(obj.p4); private 이라 사용 불가능.
+
+	}
+
+}
