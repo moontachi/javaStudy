914a311b2eec0c08fc281b46fe06f451b4001bc5
KIMMUNKWAN <KIMMUNKWAN@DESKTOP-98P9004>
2020-03-29 11:20:09 +0900

    상속

--- javaStudy/src/java0329_2/BusExam.java
+++ javaStudy/src/java0329_2/BusExam.java
@@ -6,7 +6,11 @@ public class BusExam {
 		Bus bus = new Bus();
 		bus.run();
 		bus.ppangppang();
-
+		
+		
+		Car car = new Car();
+		car.run();
+//		car.ppangppang(); <-부모클래스는 자식이 가지고 있는 것을 사용 할 수 없다.
 	}
 
 }
